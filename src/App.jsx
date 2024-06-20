5782a034bbf0cfaa03d7ea915e33bc57463cc7a0
aditigoyal291 <goyaladiti2912@gmail.com>
2024-06-20 21:56:03 +0530

    Shadcn init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import "./styles/App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
-      <div className="bg-red-500">
-        <h1 className="p-4 flex justify-center bg-yellow-300  ">pillow</h1>
+      <div className="">
+        <h1 className="p-4 flex justify-center bg-yellow-300">pillow</h1>
+        <Button>Submit</Button>
       </div>
     </>
   );
