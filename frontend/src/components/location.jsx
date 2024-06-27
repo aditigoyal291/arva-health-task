65071b6215e5c17837d71838e7bfab7cf67b04da
aditigoyal291 <goyaladiti2912@gmail.com>
2024-06-28 00:15:32 +0530

    Adding maps and user location

--- frontend/src/components/location.jsx
+++ frontend/src/components/location.jsx
@@ -34,6 +34,7 @@ const LocationPrompt = ({ isOpen, onClose }) => {
                 className="rounded bg-teal-700 px-4 py-2 font-bold text-white hover:bg-teal-800"
               >
                 Allow Location Access
+                
               </button>
               <button
                 onClick={onClose}
